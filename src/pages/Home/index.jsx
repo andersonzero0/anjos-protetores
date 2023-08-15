05333b3fd2ea609d65fa258b92ad0e07e16bd800
Anderson Viana <programadorzero@hotmail.com>
2023-08-15 07:37:03 -0300

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,11 +2,12 @@ import ComoPossoAjudar from "../../components/ComoPossoAjudar";
 import Header from "../../components/Header";
 import Historias from "../../components/Historias";
 import QuemSomos from "../../components/QuemSomos";
+import "./style.css";
 
 export default function Home() {
 
     return (
-        <>
+        <div class="main">
             <Header />
 
             <QuemSomos />
@@ -14,7 +15,7 @@ export default function Home() {
             <ComoPossoAjudar/>
 
             <Historias/>
-        </>
+        </div>
     )
 
 }
